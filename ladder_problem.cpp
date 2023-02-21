67c4020dbfacd54109ec78806b2e0201aabd277d
Kartikey Solanki <kartikey.rajput0071@gmail.com>
2023-02-21 21:26:25 +0530

    Ladder Problem Recursion

---
+++ ladder_problem.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std ;
+
+int ladder(int n, int k)
+{
+	if (n == 0)
+	{
+		return 1 ;
+	}
+
+	if (n < 0)
+	{
+		return 0 ;
+	}
+
+	int ans = 0 ;
+	for(int i = 1; i <= k; i++)
+	{
+		ans += ladder(n - i,k) ;
+	}
+
+	return ans ;
+}
+
+int main ()
+{
+	int n, k ;
+	cin>>n>>k ;
+
+	int ans = ladder(n,k) ;
+	cout<<ans ;
+
+	return 0 ;
+}
